6abf89e8a0a354b9c8812b87472f1e6a536d0e73
Islam Badwan <66380160+Malsa2000@users.noreply.github.com>
2023-03-26 03:15:01 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/pages/about_us.dart
    #	lib/pages/our_servises.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return  ScreenUtilInit(
         designSize: const Size(1820, 5024),
     minTextAdapt: true,
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(backgroundColor: Colors.white),
-        initialRoute: "/home",
+        initialRoute: size.width > 380?  "/home" :"/home_app",
         home: Home(),
         routes: {
           //home web
